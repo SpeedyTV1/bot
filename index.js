e504dec253e7381ed5bac35e4134b98407d4f348
SpeedyTV1 <47642925+SpeedyTV1@users.noreply.github.com>
2019-02-14 23:11:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,23 @@ bot.on('message', message=>{
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
+        case 'cmds':
+            const embed = new.Discord.RichEmbed()
+            .setTitle('Commands')
+            .addField('-info', 'Shows info about the server and the bot.')
+            .addField('-links', 'Displays the group link and Discord invite code')
+            .addField('-clear [n]', 'Clears inserted ammount of messages including the command.')
+        break;
         case 'execute':
             message.channel.send('Not in order.')
         break;
         case "info":
             const embed = new Discord.RichEmbed()
-            .setTitle ('Informations')
-            .addField ('Server Owner', message.guild.owner.user.username)
+            .setTitle('Informations')
+            .addField('Server Owner', message.guild.owner.user.username)
             .addField('Current Server', message.guild)
             .addField('Bot Version', version)
+            .addField('Bot Prefix', '-')
             .setThumbnail(message.guild.owner.user.avatarURL)
             .setColor(0xF1C40F)
             message.channel.send(embed);
