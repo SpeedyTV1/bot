f800ba75df9c3cf16d4dbddc3315c99b740d421e
SpeedyTV1 <47642925+SpeedyTV1@users.noreply.github.com>
2019-03-13 20:40:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ bot.on('message', message=>{
         }
         break;
         case '-files':
-            message.channel.send('Theta_Protocol\nP.E.G.A.S.U.S.\nCaptain_Marvel\nKREE\nProject_Quinjet')
+            message.channel.send('Theta_Protocol\nP.E.G.A.S.U.S.\nCaptain_Marvel\nKREE\nProject_Quinjet\nSteve_Rogers')
         break;
         case '-test':
         if(message.author.id === ownerid, coownerid){
