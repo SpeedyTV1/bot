49a407cb61c15c3f5140859ff3816bf2b6331df6
SHIELD-L-1-0-2 <48410678+fggggfbnf@users.noreply.github.com>
2019-04-01 18:52:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ bot.on('message', message=>{
                 //const embed2 = new Discord.RichEmbed()
                 //.setTitle('Database')
                 //.addField('Director', 'SpeedyTV_1')
-               // .addField('Secretary', 'fggggfbnf')
+               // .addField('Secretary', 'President1239, before fggggfbnf')
                // .addField('Deputyy Director', 'JordySpero')
               //  .setColor(0xF1C40F)
              //   message.channel.send(embed2);
